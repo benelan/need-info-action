ce5671ab32efcacd94d264c6318c72099290a618
Ben Elan <belan@esri.com>
2021-09-03 17:33:19 -0700

    initial changes

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const {repo, payload, issue} = github.context
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
+    const requireAll = core.getMultilineInput('require_all')
+    const requireOne = core.getMultilineInput('require_one')
+    const myToken = core.getInput('github_token')
+    const client = github.getOctokit(myToken)
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      core.setFailed(e.message)
+    }
   }
 }
 
