ed421695549b80f0c0c6e2e3c028728d19a0f1c7
Ben Elan <benelan@users.noreply.github.com>
2021-09-06 16:26:34 -0700

    clean up config

--- src/config.ts
+++ src/config.ts
@@ -26,50 +26,34 @@ export default class Config {
     this.commentHeader = config.commentHeader || ''
   }
 
-  isValidRequiredItem = (item: any): item is RequiredItem => {
-    if (
-      !(
-        item !== null &&
-        typeof item === 'object' &&
-        'commentBody' in item &&
-        'requireAll' in item &&
-        'content' in item &&
-        typeof item.commentBody === 'string' &&
-        typeof item.requireAll === 'boolean' &&
-        Array.isArray(item.content)
-      )
-    ) {
-      return false
-    }
-    return item.content.every((i: any) => {
-      return typeof i === 'string'
-    })
+  isValidRequiredItem = (item: any): item is RequiredItem =>
+    item !== null &&
+    typeof item === 'object' &&
+    'commentBody' in item &&
+    'requireAll' in item &&
+    'content' in item &&
+    typeof item.commentBody === 'string' &&
+    typeof item.requireAll === 'boolean' &&
+    Array.isArray(item.content)
+      ? item.content.every((i: any) => typeof i === 'string')
+      : false
+
+  isValidConfig(obj: any): boolean {
+    return obj !== null &&
+      typeof obj === 'object' &&
+      'requiredItems' in obj &&
+      'labelToAdd' in obj &&
+      'labelsToCheck' in obj &&
+      typeof obj.labelToAdd === 'string' &&
+      Array.isArray(obj.requiredItems) &&
+      Array.isArray(obj.labelsToCheck)
+      ? obj.requiredItems.every(this.isValidRequiredItem)
+      : false
   }
 
-  isValidConfig(item: any): boolean {
-    if (
-      !(
-        item !== null &&
-        typeof item === 'object' &&
-        'requiredItems' in item &&
-        'labelToAdd' in item &&
-        'labelsToCheck' in item &&
-        typeof item.labelToAdd === 'string' &&
-        Array.isArray(item.requiredItems) &&
-        Array.isArray(item.labelsToCheck)
-      )
-    ) {
-      return false
-    }
-    return item.requiredItems.every(this.isValidRequiredItem)
-  }
-
-  parseConfig(path: string): any {
-    const data = yaml.load(fs.readFileSync(path, 'utf8'))
-    if (!this.isValidConfig(data)) {
-      throw new Error('Invalid configuration, ending action.')
-    } else {
-      return data
-    }
+  parseConfig(path: string): Config {
+    const data: any = yaml.load(fs.readFileSync(path, 'utf8'))
+    if (this.isValidConfig(data)) return data
+    throw new Error('Invalid configuration, ending action.')
   }
 }
