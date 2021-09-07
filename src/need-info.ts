49221503649e755f4c38f453a07939a795cd7454
Ben Elan <belan@esri.com>
2021-09-07 13:11:54 -0700

    add methods to handle issue comments

--- src/need-info.ts
+++ src/need-info.ts
@@ -23,7 +23,7 @@ export default class NeedInfo {
     if (action === 'opened') {
       core.debug('TODO: initial content check')
     } else if (action === 'edited' || action === 'labeled') {
-      core.debug('TODO: content check if post has the label')
+      core.debug('TODO: content check if post has a label to check')
     } else {
       throw new Error(
         `Unsupported issue or pull request action: ${action}, ending run.`
@@ -31,12 +31,28 @@ export default class NeedInfo {
     }
   }
 
-  onComment(): void {
-    const {action} = github.context.payload
-    if (action === 'created' || action === 'edited') {
-      core.debug('TODO')
+  async onComment(): Promise<void> {
+    const {payload, repo} = github.context
+    if (
+      (payload.action === 'created' || payload.action === 'edited') &&
+      payload.comment !== undefined
+    ) {
+      const comment = await this.octokit.rest.issues.getComment({
+        ...repo,
+        comment_id: payload.comment.id
+      })
+      if (comment.data.body !== undefined) {
+        await this.createComment(
+          this.config.commentHeader,
+          this.getCommentBodies(comment.data.body).join('\n'),
+          this.config.commentFooter,
+          payload.comment.id
+        )
+      }
     } else {
-      throw new Error(`Unsupported comment action: ${action}, ending run.`)
+      throw new Error(
+        `The event was not a comment or the action "${payload.action}" is not supported, ending run.`
+      )
     }
   }
 
@@ -57,36 +73,32 @@ export default class NeedInfo {
 
   async hasLabelToAdd(issue: Issue): Promise<boolean> {
     const labels = await this.octokit.rest.issues.listLabelsOnIssue({...issue})
-
     return labels.data.map(label => label.name).includes(this.config.labelToAdd)
   }
 
-  hasRequiredItems(post: string): boolean {
-    return this.config.requiredItems.every(item => {
-      if (item.requireAll) {
-        return item.content.every(content => {
-          return content.toLowerCase().includes(post.toLowerCase())
-        })
-      } else {
-        return item.content.some(content => {
-          return content.toLowerCase().includes(post.toLowerCase())
-        })
-      }
-    })
+  getCommentBodies(post: string): string[] {
+    const inPost = (text: string): boolean =>
+      post.toLowerCase().includes(text.toLowerCase())
+
+    return this.config.requiredItems
+      .filter(
+        item =>
+          (item.requireAll && !item.content.every(c => inPost(c))) ||
+          (!item.requireAll && !item.content.some(c => inPost(c)))
+      )
+      .map(item => item.commentBody)
   }
 
-  getResponses(post: string): string[] {
-    const inPost = (text: string): boolean =>
-      text.toLowerCase().includes(post.toLowerCase())
-
-    const responses = []
-    for (const item of this.config.requiredItems) {
-      if (
-        (item.requireAll && !item.content.every(c => inPost(c))) ||
-        (!item.requireAll && !item.content.some(c => inPost(c)))
-      )
-        responses.push(item.commentBody)
-    }
-    return responses
+  async createComment(
+    header: string,
+    body: string,
+    footer: string,
+    issue_number: number
+  ): Promise<void> {
+    await this.octokit.rest.issues.createComment({
+      ...github.context.repo,
+      issue_number,
+      body: `${header}\n${body}\n${footer}`
+    })
   }
 }
