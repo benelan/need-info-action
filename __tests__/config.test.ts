6a00b1cf8541c33264b4241e461f3ed4fb1dd25d
Ben Elan <benelan@protonmail.com>
2021-09-06 18:22:45 -0700

    update yml

--- __tests__/config.test.ts
+++ __tests__/config.test.ts
@@ -3,6 +3,6 @@ import Config from '../src/config'
 
 
 test('initialize Config and check options', () => {
-  const config = new Config('.github/need-info-config.yml')
+  const config = new Config('.github/need-info.yml')
   expect(config.labelToAdd).toEqual('need more info')
 })
